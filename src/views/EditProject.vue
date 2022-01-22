609e502751ffe0163dc82bf2ff613daba171fe32
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-01-22 21:16:25 +0300

    компонент с формой редактирования проекта

---
+++ src/views/EditProject.vue
@@ -0,0 +1,73 @@
+<template>
+  <form>
+    <label for="title">Title: </label>
+    <input type="text" id="title" required v-model="title" />
+    <label for="details">Details</label>
+    <textarea id="details" required v-model="details"></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+<script>
+export default {
+  props: {
+    id: Number,
+  },
+  data() {
+    return {
+      title: '',
+      details: '',
+      uri: `http://localhost:3000/projects/${this.id}`,
+    };
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        this.title = data.title;
+        this.details = data.details;
+      })
+      .catch((e) => console.log(e));
+  },
+};
+</script>
+<style lang="scss" scoped>
+form {
+  background: white;
+  padding: 20px;
+  border-radius: 10px;
+}
+label {
+  display: block;
+  color: #bbb;
+  text-transform: uppercase;
+  font-size: 14px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin: 20px 0 10px 0;
+}
+input,
+textarea {
+  padding: 10px;
+  width: 100%;
+  box-sizing: border-box;
+}
+input {
+  border: 0;
+  border-bottom: 1px solid #ddd;
+}
+textarea {
+  border: 1px solid #ddd;
+  padding: 10px;
+  height: 100px;
+}
+form button {
+  display: block;
+  margin: 20px auto 0;
+  background: #00ce89;
+  color: white;
+  padding: 10px;
+  border: 0;
+  border-radius: 6px;
+  font-size: 16px;
+}
+</style>
