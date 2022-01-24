eba1b941a749cdc3a4f81d0043616f173e5e3482
Mark Nelyubin <m.nelyubin@sredaobuchenia.ru>
2022-01-24 10:14:45 +0300

    настроил, чтобы работали фильтры по проектам

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <FilterNav @onFilter="current = $event" :current="current" />
     <div v-if="projects.length">
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
       </div>
     </div>
@@ -25,36 +25,26 @@ export default {
       current: 'all',
     };
   },
+  computed: {
+    filteredProjects() {
+      switch (this.current) {
+        case 'completed':
+          return this.projects.filter((project) => project.complete);
+        case 'ongoing':
+          return this.projects.filter((project) => !project.complete);
+        default:
+          return this.projects;
+      }
+    },
+  },
   methods: {
     handleDelete(id) {
       this.projects = this.projects.filter((project) => project.id !== id);
     },
     handleComplete(id) {
-      // this.projects = this.projects.map((project) => {
-      //   if (project.id === id) {
-      //     const updatedProject = { ...project, complete: !project.complete };
-      //     return updatedProject;
-      //   }
-      //   return project;
-      // });
       const p = this.projects.find((project) => project.id === id);
       p.complete = !p.complete;
     },
-    // filterProjects(category) {
-    //   switch (category) {
-    //     case 'all':
-    //       console.log('all');
-    //       break;
-    //     case 'completed':
-    //       console.log('completed');
-    //       break;
-    //     case 'ongoing':
-    //       console.log('ongoing');
-    //       break;
-    //     default:
-    //       break;
-    //   }
-    // },
   },
   mounted() {
     fetch('http://localhost:3000/projects')
